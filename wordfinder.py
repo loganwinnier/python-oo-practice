ff740db55ce1906995ac3d696c5f3635d85b4d91
John Flores <johnflores1991@hotmail.com>
2023-09-26 18:12:02 -0500

    instantiated WordFinder class

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,21 @@
 class WordFinder:
-    """Word Finder: finds random words from a dictionary."""
+    """Word Finder: finds random words from a dictionary.
+    >>> new_word = WordFinder('/usr/share/dict/words')
+
+    >>> new_word.word_reader('/usr/share/dict/words')
+    True
+    """
+    def __init__(self, path):
+        self.words = self.word_reader(path)
+
+    def word_reader(self, path):
+        words = []
+        open_file = open(path, 'r')
+        for line in open_file:
+            words.append(line[:-2])
+        open_file.close()
+        print(f'{len(words)} words read')
+        return words
+
+    def random(self):
+        pass
