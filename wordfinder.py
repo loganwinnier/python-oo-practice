eea4b138e3b318deb0f6dfd4b817d6ce09d21a7c
John Flores <johnflores1991@hotmail.com>
2023-09-26 19:44:09 -0500

    completed wordfinder() functionality and added some test text files

--- wordfinder.py
+++ wordfinder.py
@@ -3,25 +3,39 @@ from random import choice as random_choice
 
 class WordFinder:
     """Word Finder: finds random words from a dictionary.
-    >>> new_word = WordFinder('/usr/share/dict/words')
-
-    >>> new_word.word_reader('/usr/share/dict/words')
-    True
     """
 
+    # for testing: '/usr/share/dict/words'
+
     def __init__(self, path):
+        """creates word reader from input file and prints # of words read"""
+        print('inside parent __init__')
         self.words = self.word_reader(path)
+        print(f'{len(self.words)} words read')
+
+    def __repr__(self):
+        return f'<WordFinder self.words={self.words}>'
 
     def word_reader(self, path):
-        words = []
+        """reads input file line-by-line and creates list with each line read
+        """
+        print('inside parent word_reader')
         open_file = open(path, 'r')
-        for line in open_file:
-            words.append(line[:-2])
-        open_file.close()
-        print(f'{len(words)} words read')
-        return words
+        return [word.strip() for word in open_file]
 
     def random(self):
+        """returns random word from input word list
+        """
         ran_word = random_choice(self.words)
         print(ran_word)
         return ran_word
+
+class SpecialWordFinder(WordFinder):
+    ''''''
+
+    def word_reader(self, path):
+        print('inside child word_reader')
+        words = super().word_reader(path)
+        return [word for word in words if not word.startswith('#') and word != '']
+
+
